90d58a39aa562bec81f60c2f1f453b7cfe6d2575
Zakhar Mitrofanov <sibaraneae@gmail.com>
2022-04-11 12:54:23 +0300

    4

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,63 @@
-const gulp = require('gulp');
+const { src, dest, series, watch, parallel } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('taskName', () => {
-    return gulp.src('source-files') // Выборка исходных файлов для обработки плагином
-        .pipe(plugin()) // Вызов Gulp плагина для обработки файла
-        .pipe(gulp.dest('folder')); //Вывод результата в папку назначения dest (destination - пункт назначения)
-});
+function buildSass() {
+    return src('src/scss/**/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ includePaths: ['./node_modules'] }).on('error', sass.logError))
+        .pipe(
+            postcss([
+                autoprefixer({
+                    grid: true,
+                    overrideBrowserslist: ['last 2 versions']
+                }),
+                cssnano()
+            ])
+        )
+        .pipe(sourcemaps.write())
+        .pipe(dest('src/css'))
+        .pipe(dest('dist/css'))
+        .pipe(browserSync.stream());
+}
+
+// Таск работы с html файлами
+function buildHtml() {
+    return src('src/**/*.html')
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream());;
+}
+
+// Таск копирования статичных файлов
+function copy() {
+    return src(['src/images/**/*.*'], { base: 'src' })
+        .pipe(dest('dist'))
+        .pipe(browserSync.stream());;
+}
+
+// Таск очистки dist
+function cleanDist() {
+    return del('dist');
+}
+
+// Таск отслеживания изменения файлов
+function serve() {
+    watch('src/scss/**/*.scss', buildSass);
+    watch('src/**/*.html', buildHtml);
+}
+
+// Создание дев-сервера
+function createDevServer() {
+    browserSync.init({
+        server: 'src',
+        notify: false
+    });
+}
+
+exports.build = series(cleanDist, buildSass, buildHtml, copy);
+exports.default = series(buildSass, parallel(createDevServer, serve));
